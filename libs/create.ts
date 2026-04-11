83a7231e7048a58c84746a17bf122d2cbea72450
xiaoxustudio <xiaoxustudio@foxmail.com>
2026-04-11 13:15:34 +0800

    feat: add prettier config and jsonModify utility

--- libs/create.ts
+++ libs/create.ts
@@ -4,6 +4,8 @@ import ora from "ora";
 import { templates } from "./templates";
 import { validateProjectName } from "./helpers";
 import { create } from "create-tsdown";
+import { copyFile, fstat } from "fs";
+import { jsonModify } from "./utils";
 
 export default async function createProject(projectName: string) {
 	validateProjectName(projectName);
@@ -30,6 +32,25 @@ export default async function createProject(projectName: string) {
 		} else {
 			await create(projectName, {});
 		}
+
+		// 替换prettier配置
+		copyFile(
+			"config/prettier/.prettierrc.json",
+			`${projectName}/.prettierrc.json`,
+			(err) => {
+				if (err) throw err;
+			}
+		);
+		copyFile(
+			"config/prettier/.prettierignore",
+			`${projectName}/.prettierignore`,
+			(err) => {
+				if (err) throw err;
+			}
+		);
+		jsonModify(`${projectName}/package.json`, {
+			devDependencies: { prettier: "^3.8.2" }
+		});
 
 		spinner.succeed("Project files created");
 		console.log(chalk.green.bold("\n🎉 Project created successfully!"));
--- libs/utils.ts
+++ libs/utils.ts
@@ -1,3 +1,5 @@
+import { readFile, writeFile } from "fs";
+
 export interface CopyableOption {
 	name: string;
 	actions: Record<string, string>[];
@@ -5,4 +7,20 @@ export interface CopyableOption {
 
 export function defineCopyable<T extends CopyableOption>(obj: T): T {
 	return Object.assign({}, obj);
+}
+
+export function jsonModify(path: string, needed: any) {
+	readFile(path, "utf-8", (err, data) => {
+		if (err) throw err;
+		const json = JSON.parse(data);
+		for (const key in needed) {
+			if (json[key]) {
+				json[key] = needed[key];
+			}
+		}
+		console.log("徐然", json);
+		writeFile(path, JSON.stringify(json, null, 2), (err) => {
+			if (err) throw err;
+		});
+	});
 }
